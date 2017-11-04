4891a6198dcd2447d8ef7509a8f9bce4c7eff9c0
anima-pa <anima1111@outlook.com>
2017-11-04 11:56:34 +0530

    Add project files.

---
+++ PushNotify/PushNotify.Android/MainApplication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Plugin.PushNotification;
+
+namespace PushNotify.Droid
+{
+    [Application]
+   public class MainApplication:Application
+    {
+        protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            PushNotificationManager.Initialize(this, true);
+#else
+              PushNotificationManager.Initialize(this,false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossPushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+
+
+            };
+        }
+        }
+}
